7b1d4e28f9627a4f5a28f30121ef00c477e2bee7
Tr4nki <niqui6@hotmail.com>
2020-05-04 00:34:32 +0200

    Update testRequire.js

--- testRequire.js
+++ testRequire.js
@@ -1,3 +1,12 @@
+// @name TestScript
+// @description Awesome description
+// @copyright 2020, Tr4nki (https://openuserjs.org/users/Tr4nki)
+// @license MIT
+// @homepageURL https://openuserjs.org/meta/Tr4nki/My_Script.meta.js
+// @supportURL https://openuserjs.org/meta/Tr4nki/My_Script.meta.js
+// @version 1.0.0
+// @updateURL https://openuserjs.org/meta/Tr4nki/My_Script.meta.js
+// 
 var ObjTest={a:1,b:2};
 
 !function(){
